361cc49e289b6b871f8bf8a6fc2c01c603bb5fdf
default <default@localhost.localdomain>
2019-01-18 08:40:29 +0100

    fixed a lot of stuff

--- deletable_primes.cpp
+++ deletable_primes.cpp
@@ -11,14 +11,55 @@
  * ----------------------------------------------------------
  */
 
-unsigned long remove_digit(int index, unsigned long n){
-    return 0;
+#include "deletable_primes.h"
+#include <math.h>
+
+bool is_prime_number(unsigned long number)
+{
+    if (number == 1) return false;
+
+    for (int i = 2; i <= sqrt(number); i++)
+    {
+        if (number % i == 0) return false;
+    }
+    return true;
 }
 
-int get_ways(unsigned long p){
-    return 0;
+int get_length(unsigned long number)
+{
+    int length = 0;
+    while (number != 0)
+    {
+        number /= 10;
+        length++;
+    }
+    return length;
 }
 
-bool is_prime(unsigned long p){
-    return false;
+unsigned long remove_digit(int i, unsigned long number)
+{
+    unsigned long left = 0
+    unsigned long right = 0;
+    
+    if (i == 0) return number / 10;
+
+    left  = number / pow(10, i + 1);
+    right = number % (unsigned long) pow(10, i);
+
+    return left * pow(10, i) + right;
+}
+
+int get_ways(unsigned long number)
+{
+    int possible_ways = 0;
+    for (int i = 0; i < get_length(number); i++)
+    {
+        if (get_length(number) == 1 && is_prime_number(number)) return 1;
+        
+        if (is_prime_number(remove_digit(i, number)))
+        {
+            possible_ways += get_ways(remove_digit(i, number));
+        }
+    }
+    return possible_ways;
 }
